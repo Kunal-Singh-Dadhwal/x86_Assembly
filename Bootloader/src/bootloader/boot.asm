c04781f7c785318c30cca525001c3affd98a414a
Kunal-Singh-Dadhwal <155142500+Kunal-Singh-Dadhwal@users.noreply.github.com>
2024-07-08 20:19:59 +0530

    Disk read

--- Bootloader/src/bootloader/boot.asm
+++ Bootloader/src/bootloader/boot.asm
@@ -4,7 +4,7 @@ BITS 16
 jmp short main
 nop
 
-bdb_oem: db "MSWIN4.1"
+bdb_oem: db "MSWIN4.1"          ;Disk Headers
 bdb_byte_per_sector: dw 512
 bdb_sectors_per_cluster: db 1
 bdb_reserved_sectors: dw 1
@@ -18,6 +18,7 @@ bdb_heads: dw 2
 bdb_hidden_sectors: dd 0
 bdb_large_secotr_count: dd 0
 
+;extended boot record
 ebr_drive_number:   db 0
                     db 0
 ebr_signature: db 29h
@@ -31,6 +32,13 @@ main:
     mov ss, ax
 
     mov sp, 0x7C00
+
+    mov [ebr_drive_number], dl      ;Reading disk sector
+    mov al, 1
+    mov cl, 1
+    mov bx, 0x7E00
+    call disk_read
+
     mov si, boot_msg
     call print
 
@@ -39,6 +47,75 @@ main:
 halt:
     jmp halt
 
+;input LBA index in ax
+;cx [bits 0 - 5]: sector number
+;cx [bits 6 - 15]: cylinder
+;dh: head
+
+lba_to_chs:
+    push ax
+    push dx
+    xor dx, dx
+    div word [bdb_sector_per_track]     ;(LBA % sector_per_track) + 1 = sector 
+    inc dx
+    mov cx, dx
+
+    xor dx, dx
+    div word [bdb_heads]
+    
+    mov dh, dl      ;head: (LBA/ sector per track) % number of heads
+    mov cl, al
+    shl ah, 6
+    or cl, ah       ;cylinder: (LBA/ sector per track)/ number of heads
+    
+    pop ax
+    mov dl, al
+    pop ax
+    ret
+disk_read:
+    push ax
+    push bx
+    push cx
+    push dx
+    push di
+
+    call lba_to_chs
+    mov ah, 2
+
+    mov dl, 3       ;counter
+
+retry:
+    stc
+    int 13h
+    jnc done_read
+    
+    call disk_reset
+    dec di
+    test di, di
+    jnz retry
+
+fail_disk_read:
+    mov si, read_failure
+    call print
+    hlt
+    jmp halt
+
+disk_reset:
+    pusha
+    mov ah, 0
+    stc
+    int 13h
+    jc fail_disk_read
+    popa
+    ret
+
+done_read:
+    pop di
+    pop dx
+    pop cx
+    pop bx 
+    pop ax
+    ret
 print:
     push si
     push ax
@@ -63,4 +140,5 @@ done_print:
 
 boot_msg: db "Our OS has booted!", 0x0D, 0x0A, 0
 TIMES 510-($-$$) db 0
+read_failure db "Failed to read disk", 0x0D, 0x0A, 0
 DW 0AA55h 
