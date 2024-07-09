06d30bafaf27dd4c4efb357e25f777cee71829ca
Kunal-Singh-Dadhwal <155142500+Kunal-Singh-Dadhwal@users.noreply.github.com>
2024-07-09 12:51:04 +0530

    Loading from kernel

--- Bootloader/src/bootloader/boot.asm
+++ Bootloader/src/bootloader/boot.asm
@@ -41,9 +41,118 @@ main:
 
     mov si, boot_msg
     call print
+    
+    ;4 segments
+    ;reserved_sector: 1
+    ;FAT 18 sectors
+    ;root dir
+    ;Data
+
+    mov ax, [bdb_sectors_per_fat]
+    mov bl, [bdb_fat_count]
+    xor bh, bh
+    mul bx
+    add ax, [bdb_reserved_sectors]  ;LBA of root
+    push ax
+
+    mov ax, [bdb_dir_entries_count]
+    shl ax, 5
+    xor dx, dx
+    div word [bdb_byte_per_sector]  ;(32*num of entries)/bytes per sector
+    test dx, dx
+    jz root_dir_after
+    inc ax
+
+
+root_dir_after:
+    mov cl, al
+    pop ax
+    mov dl, [ebr_drive_number]
+    mov bx, buffer
+    call disk_read
+
+    xor bx, bx
+    mov di, buffer
+    
+search_kernel:
+    mov si, file_kernel_bin
+    mov cx, 11
+    push di
+    repe cmpsb
+    pop di
+    je foundkernel
+    
+    add di, 32
+    inc bx
+    cmp bx, [bdb_dir_entries_count]
+    jl search_kernel
+
+    jmp kernelnotfound
+
+kernelnotfound: 
+    mov si, msg_kernel_not_found
+    call print
+    hlt 
+    jmp halt
+
+foundkernel:
+    mov ax, [di+26]
+    mov [kernel_cluster], ax
+    
+    mov ax, [bdb_reserved_sectors]
+    mov bx, buffer
+    mov cl, [bdb_sectors_per_fat]
+    mov dl, [ebr_drive_number]
+    call disk_read
+    mov bx, kernel_load_segment
+    mov es, bx
+    mov bx, kernel_load_offset
+
+loadkernelloop:
+    mov ax, [kernel_cluster]
+    add ax, 31
+    mov cl, 1
+    mov dl, [ebr_drive_number]
+    call disk_read
+    add bx, [bdb_byte_per_sector]
+
+    mov ax, [kernel_cluster]    ;(kernel_cluster * 3)/2
+    mov cx, 3
+    mul cx 
+    mov cx, 2
+    div cx
+
+    mov si, buffer
+    add si, ax
+    mov ax, [ds:si]
+
+    or dx, dx
+    jz even
+
+odd: 
+    shr ax, 4
+    jmp nextclusterafter
+
+even:
+    and ax, 0x0FFF
+
+nextclusterafter:
+    cmp ax, 0x0FF8
+    jae readFinish
+
+    mov [kernel_cluster], ax
+    jmp loadkernelloop
+
+readFinish:
+    mov dl, [ebr_drive_number]
+    mov ax, kernel_load_segment
+    mov ds, ax
+    mov ex, ax
+
+    jmp kernel_load_segment:kernel_load_offset
 
     hlt
-
+    
 halt:
     jmp halt
 
@@ -140,5 +249,15 @@ done_print:
 
 boot_msg: db "Our OS has booted!", 0x0D, 0x0A, 0
 TIMES 510-($-$$) db 0
+file_kernel_bin db "KERNEL  BIN"
+msg_kernel_not_found db "KERNEL.BIN not found!"
+kernel_cluster dw 0
+
+kernel_load_segment equ 0x2000
+kernel_load_offset equ 0
+
 read_failure db "Failed to read disk", 0x0D, 0x0A, 0
 DW 0AA55h 
+
+buffer: 
+    
